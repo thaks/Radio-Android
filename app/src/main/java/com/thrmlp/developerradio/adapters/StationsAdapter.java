4955536d53edb0531ae2c0064aee8f21651a8f7e
mohammed thahir <thrmlp@gmail.com>
2016-09-01 11:18:41 +0530

    Added Station Fragment , Added Recyclerview and its adapter, and view Holder

---
+++ app/src/main/java/com/thrmlp/developerradio/adapters/StationsAdapter.java
@@ -0,0 +1,26 @@
+package com.thrmlp.developerradio.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.thrmlp.developerradio.holders.StationViewHolder;
+
+/**
+ * Created by a on 01/09/2016.
+ */
+public class StationsAdapter extends RecyclerView.Adapter<StationViewHolder> {
+    @Override
+    public StationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(StationViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/thrmlp/developerradio/holders/StationViewHolder.java
@@ -0,0 +1,13 @@
+package com.thrmlp.developerradio.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by a on 01/09/2016.
+ */
+public class StationViewHolder extends RecyclerView.ViewHolder{
+    public StationViewHolder(View itemView) {
+        super(itemView);
+    }
+}
